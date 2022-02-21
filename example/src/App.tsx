a7fb54427e1eabb3ea47e722068d5a15600a1350
Leri Gogsadze <lgogsadze93@gmail.com>
2022-02-21 19:41:55 +0400

    add native dependencies

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,20 @@
-import * as React from 'react';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-corner-video';
+const App = () => {
+    return (
+        <View style={styles.container}>
 
-export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
-  );
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 });
+
+export default App;
